4f3eccc49af18ce54fd5110fdaaea9efa250e1b3
Jesus Sanchez Delgado <jesuszzaaqq@hotmail.com>
2025-05-03 23:01:49 +0200

    Add interceptor

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,12 +1,14 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
+import { authInterceptor } from './app/auth.interceptor';
+
 
 bootstrapApplication(AppComponent, {
   ...appConfig,
-  providers: [...(appConfig.providers || []), provideHttpClient(), provideAnimationsAsync(), provideRouter(routes)]
+  providers: [...(appConfig.providers || []), provideHttpClient(withInterceptors([authInterceptor])), provideAnimationsAsync(), provideRouter(routes)]
 }).catch(err => console.error(err));
